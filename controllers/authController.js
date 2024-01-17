fddf469e4f1e0634e590ba6f0f4968a2df555529
David Olowomeye <olowomeyedavid242@gmail.com>
2024-01-17 01:57:24 +0000

    Created the login api route

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,8 +2,11 @@ const UserModel = require("../models/UserModel");
 // For password encryption
 const bcryptjs = require("bcryptjs");
 // For handling errors
-const errorHandler = require("../utils/Error")
+const errorHandler = require("../utils/Error");
+// To handle cookies
+const jwt = require("jsonwebtoken");
 
+// For Registration
 const register = async (req, res, next) => {
   const { username, email, password } = req.body;
   //   console.log(req.body);
@@ -22,4 +25,29 @@ const register = async (req, res, next) => {
   }
 };
 
+// For Login
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await UserModel.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    // To compare password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword)
+      return next(errorHandler(401, "Incorrect email or password!"));
+    // To handle cookie
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    // To send other user info except the password to the client for security reasons
+    const { password: passwd, ...otherInfo } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(otherInfo);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = register;
+module.exports = login;
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const register = require("../controllers/authController.js");
+const login = require("../controllers/authController.js");
 
 const authRouter = express.Router();
 
 authRouter.post("/register", register);
+authRouter.post("/login", login);
+
 
 module.exports = authRouter;
